7a20955856875889340451dc968208c1bb00e4b3
Emanuel <emamareli97@gmail.com>
2020-10-26 00:43:57 -0300

    Added movement and shooting mechanics

---
+++ Inteligencia Artificial Parcial 2/Assets/Script/GunController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public float damage = 30f;
+    public float range = 100f;
+    public float impactForce = 50f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFash;
+    public GameObject impactEffect;
+    public LayerMask targetLayers;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("pium pium!");
+            Shoot();
+        }
+
+        void Shoot()
+        {
+           //Debug.Log("Shot");
+            muzzleFash.Play();
+            RaycastHit hit;
+            if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward,out hit, range, targetLayers))
+            {
+                //Debug.Log("Choque!");
+                Target target = hit.transform.GetComponent<Target>();
+                if (target != null)
+                {
+                    Debug.Log(hit.transform.name);
+                    target.TakeDamage(damage);
+                }
+                if(hit.rigidbody != null)
+                {
+                    //Debug.Log("Alta fuerza loco");
+                    hit.rigidbody.AddForce(-hit.normal * impactForce);
+                }
+            }
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+}
---
+++ Inteligencia Artificial Parcial 2/Assets/Script/Target.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public delegate void EnemyKilled();
+    public static event EnemyKilled OnEnemyKilled;
+
+    public float health = 100f;
+    void Start()
+    {
+        //setRigidbodyState(true);
+        //setColliderState(false);
+        //GetComponent<Animator>().enabled = true;
+    }
+
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        Debug.Log(health,this);
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        //GetComponent<Animator>().enabled = false;
+        //setRigidbodyState(false);
+        //setColliderState(true);
+        Destroy(gameObject);// agregar timer en caso de usar enemigos con ragdoll
+        if (OnEnemyKilled != null)
+        {
+            OnEnemyKilled();
+        }
+    }
+   /* void setRigidbodyState(bool state)
+    {
+
+        Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody rigidbody in rigidbodies)
+        {
+            rigidbody.isKinematic = state;
+        }
+
+        GetComponent<Rigidbody>().isKinematic = !state;
+
+    }
+
+
+    void setColliderState(bool state)
+    {
+
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = state;
+        }
+
+        GetComponent<Collider>().enabled = !state;
+
+    }*/
+}
